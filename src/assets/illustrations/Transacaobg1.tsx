4842d4c64b39949eb22abdc64b46fcd50c887965
Roberta Marques <roberta.marques02@hotmail.com>
2025-06-01 17:38:13 -0300

    Corrigindo

--- src/assets/illustrations/Transacaobg1.tsx
+++ src/assets/illustrations/Transacaobg1.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Transacaobg1 = ({ className }: { className: string }) => (
 
-    <svg width="178"
+    <svg className={className} width="178"
         height="180"
         viewBox="0 0 178 180"
         fill="none"
