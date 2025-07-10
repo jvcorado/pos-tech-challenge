b0b4342265addbb7a0b3c5875c021663cad61365
coelhorafaela <coelho_rafaela@hotmail.com>
2025-07-09 21:28:14 -0300

    add transaction subtype

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,10 +1,11 @@
-import { TransactionType } from "./TransactionType";
+import { TransactionSubtype, TransactionType } from "./TransactionType";
 
 interface TransactionJSON {
   id?: number;
   description: string;
   amount: number;
   type: TransactionType;
+  subtype: TransactionSubtype;
   date: string | Date;
 }
 
@@ -13,18 +14,21 @@ export class Transaction {
   public description: string;
   public amount: number;
   public type: TransactionType;
+  public subtype: TransactionSubtype;
   public date: Date;
 
   constructor(
     description: string,
     amount: number,
     type: TransactionType,
+    subtype: TransactionSubtype,
     id?: number,
     date?: Date
   ) {
     this.description = description;
     this.amount = amount;
     this.type = type;
+    this.subtype = subtype;
     if (id !== undefined) this.id = id;
     this.date = date ?? new Date();
   }
@@ -34,6 +38,7 @@ export class Transaction {
       data.description,
       data.amount,
       data.type,
+      data.subtype,
       data.id,
       new Date(data.date)
     );
--- src/models/TransactionType.ts
+++ src/models/TransactionType.ts
@@ -1,6 +1,14 @@
 // models/TransactionType.ts
 export enum TransactionType {
-    INCOME = "INCOME", // Entrada
-    EXPENSE = "EXPENSE", // Saída
-  }
-  
+  INCOME = "INCOME", // Entrada
+  EXPENSE = "EXPENSE", // Saída
+}
+
+export enum TransactionSubtype {
+  DOC_TED = "DOC_TED",
+  BOLETO = "BOLETO",
+  CAMBIO = "CAMBIO",
+  EMPRESTIMO = "EMPRESTIMO",
+  DEPOSITO = "DEPOSITO",
+  TRANSFERENCIA = "TRANSFERENCIA",
+}
